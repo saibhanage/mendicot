cc48ae7cd703ddca3804e9a8852771f947eeda90
Sai Bhanage <sbhanage58@gmail.com>
2026-03-03 02:58:11 +0530

    Updated server CORS origin to allow live connections

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] }
+  cors: {
+    origin: "*", // <-- CHANGE THIS FROM LOCALHOST TO "*"
+    methods: ["GET", "POST"]
+  }
 });
 
 const rooms = {};
